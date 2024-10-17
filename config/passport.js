bf8988cfd5baf722fe942304bc16f28d45c05cc8
Peter Key <63628694+peterekey@users.noreply.github.com>
2024-10-18 08:43:36 +1100

    Step 6: Handle the scenario where user is found, but password is invalid

--- config/passport.js
+++ config/passport.js
@@ -9,6 +9,7 @@ passport.use(new LocalStrategy(
       helper.findByUsername(username, (err, user) => {
         if(err) return done(err);
         if(!user) return done(null, false);
+        if(user.password != password) return done(null, false);
       })
     }
   ));
